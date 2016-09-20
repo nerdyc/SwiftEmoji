c17f993fc02c571643c2646a27f6f36ad68704b1
Long Nguyen <long.nguyen@rayburst.com>
2016-09-19 23:59:35 -0700

    Swift 3 conversion

--- Sources/Emoji.swift
+++ Sources/Emoji.swift
@@ -10,19 +10,19 @@ public final class Emoji : EmojiData {
     ///
     public static var SingleEmojiPattern:String = {
         // The "emoji" group needs to be followed by a special character to be rendered like emoji.
-        let emojiVariants = "(?:(?:\(EmojiPatterns.joinWithSeparator("|")))\\uFE0F{0,1})"
+        let emojiVariants = "(?:(?:\(EmojiPatterns.joined(separator: "|")))\\uFE0F{0,1})"
         
         // Emoji can be followed by optional combining marks. The standard says only keycaps and
         // backslash are likely to be supported.
         let combiningMarks = "[\\u20E3\\u20E0]"
         
         // "Presentation" characters are rendered as emoji by default and need no variant.
-        let emojiPresentation = "\(EmojiPresentationPatterns.joinWithSeparator("|"))"
+        let emojiPresentation = "\(EmojiPresentationPatterns.joined(separator: "|"))"
         
         // Some other emoji are sequences of characters, joined with 'Zero Width Joiner' characters.
         // We want the longest match, so we sort these in reverse order.
-        let zwjSequences = ZWJSequencePatterns.reverse().joinWithSeparator("|")
-        let otherSequences = SequencePatterns.joinWithSeparator("|")
+        let zwjSequences = ZWJSequencePatterns.reversed().joined(separator: "|")
+        let otherSequences = SequencePatterns.joined(separator: "|")
         
         return "(?:(?:\(zwjSequences)|\(otherSequences)|\(emojiVariants)|\(emojiPresentation))\(combiningMarks)?)"
     }()
@@ -81,9 +81,9 @@ public final class Emoji : EmojiData {
     /// Returns `true` if the given string is composed solely of emoji and (optional) whitespace.
     /// Empty or blank strings will not match.
     ///
-    public static func isPureEmojiString(string:String) -> Bool {
+    public static func isPureEmojiString(_ string:String) -> Bool {
         let range = NSRange(location: 0, length: string.utf16.count)
-        let firstMatch = PureEmojiAndWhitespaceRegex.rangeOfFirstMatchInString(string, options:[], range:range)
+        let firstMatch = PureEmojiAndWhitespaceRegex.rangeOfFirstMatch(in: string, options:[], range:range)
         return firstMatch.location != NSNotFound
     }
     
